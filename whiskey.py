2d2262bf0c84b66fe4ca1d8defb80392434b0483
Martijn Arts <arts.martijn@gmail.com>
2016-01-27 18:43:46 +0100

    Update for new Whatsapp, Dutch version

--- whiskey.py
+++ whiskey.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python3
+from os import path
 import re
-import random
-import hashlib
 from datetime import datetime
-from calendar import month_abbr
 import json
 
 from namify import namify
 
-TIME_RE = re.compile(r'([A-Z][a-z]{2}) (\d+), (\d{2}):(\d{2})')
-AUTHOR_RE = re.compile(r'([A-Z][a-z]+) ([a-z ]*[A-Z]\w+):?')
+TIME_RE = re.compile(r'(?P<day>\d{1,2})-(?P<month>\d{1,2})-(?P<year>\d{4}), '
+                     r'(?P<hour>\d{2}):(?P<minute>\d{2})')
 PHONE_RE = re.compile(r'(\+[\d ]+\d)')
 
 
+ACTIONS = ['created', 'added', 'hebt', 'heeft']
+
+
 class Whiskey(object):
-    def __init__(self, filename):
+    def __init__(self, root, filename):
+        self.root = root
         self.filename = filename
         self.messages = []
-
 
     def _namify_match(self, match):
         return namify(match.groups()[0])
 
-
     def read(self):
         n = 0
-        with open(self.filename, 'r') as f:
+        with open(path.join(self.root, self.filename), 'r') as f:
             for line in f:
                 line = line.replace(b"\xe2\x80\xaa".decode(), '')
                 line = line.replace(b"\xe2\x80\xac".decode(), '')
@@ -33,45 +33,46 @@ class Whiskey(object):
                 line = re.sub(PHONE_RE, self._namify_match, line)
 
                 msg = {}
+
+                # Parse time
                 time = re.match(TIME_RE, line)
                 if time is None:
-                    self.messages[n-1]['text'] += line
+                    self.messages[n-1]['text'] += '\n' + line[:-1]
                     continue
 
-                g = time.groups()
-                ma = {v: k for k,v in enumerate(month_abbr)}
                 dt = datetime(
-                    2014,
-                    int(ma[g[0]]),
-                    int(g[1]),
-                    int(g[2]),
-                    int(g[3])
+                    int(time.group('year')),
+                    int(time.group('month')),
+                    int(time.group('day')),
+                    int(time.group('hour')),
+                    int(time.group('minute'))
                 )
                 msg['datetime'] = dt.isoformat()
-                msg['time'] = line[0:time.end(4)]
+                msg['time'] = line[0:time.end(5)]
 
-                rest = line[time.end(4) + 3:]
+                rest = line[time.end(5) + 3:]
 
-                if rest.lower().startswith('you'):
-                    msg['author'] = 'you'
-                    before = rest[:3]
-                    rest = rest[3:]
-                else:
-                    author = re.match(AUTHOR_RE, rest.lstrip())
-                    msg['author'] = author.groups()[0]
+                # Find message type
+                msg['type'] = 'chat'
 
-                    before = rest[:author.end(2)]
-                    rest = rest[author.end(2):]
+                split = rest.find(':')
+                for action in ACTIONS:
+                    if action in rest and (split == -1 or
+                                           rest.find(action) < split):
+                        msg['type'] = 'action'
+                        msg['author'] = rest[:rest.find(action)].strip()
 
-                if rest[0] == ':':
-                    msg['type'] = 'chat'
-                    rest = rest[2:]
-                else:
-                    msg['type'] = 'action'
+                        split = rest.find(action)
 
-                msg['text'] = rest[:-1]
+                if msg['type'] == 'chat':
+                    msg['author'] = rest[:split].strip()
+                    split += 2
+
+                # Find message
+                msg['text'] = rest[split:-1]
+
+                print(msg)
                 self.messages.append(msg)
-
 
     def write_json(self, outfile):
         with open(outfile, 'w') as f:
@@ -80,12 +81,13 @@ class Whiskey(object):
 
 if __name__ == '__main__':
     import argparse
-    parser = argparse.ArgumentParser(description="Parse a Whatsapp history file.")
+    parser = argparse.ArgumentParser(
+        description="Parse a Whatsapp history file.")
     parser.add_argument('in_file', help="the history file")
     parser.add_argument('out_file', help="a json file to write to")
 
     args = parser.parse_args()
 
-    glass = Whiskey(args.in_file)
+    glass = Whiskey(*path.split(args.in_file))
     glass.read()
     glass.write_json(args.out_file)
